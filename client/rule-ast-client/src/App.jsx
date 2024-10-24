8e6e46eb7585d0cf3acf2bbd3e5b57431eae6d56
Manikandan S <mail2aj19@gmail.com>
2024-10-24 05:36:19 +0530

    fix: added evaluation

--- client/rule-ast-client/src/App.jsx
+++ client/rule-ast-client/src/App.jsx
@@ -20,7 +20,8 @@ const ASTNode = ({ node }) => {
 function App() {
   const BACKEND_URL = 'http://127.0.0.1:5000'
   const [input, setInput] = useState("");
-  const [ast, setAst] = useState({})
+  const [ast, setAst] = useState("");
+  const [data, setData] = useState("");
   // const ast = {
   //   value: "AND",
   //   left: {
@@ -48,16 +49,29 @@ function App() {
       {headers : { 'Content-Type' : 'application/json'}}
     )
     setAst(JSON.parse(res.data.ast_json))
+  }
 
-    console.log(typeof ast)
+  const ruleEvaluateHandler = async () => {
+    const res = await axios.post(
+      `${BACKEND_URL}/evaluate_rule`, {'rule' : input, 'data': data},
+      {headers : { 'Content-Type' : 'application/json'}}
+    )
+
+    alert(`The Rule evaluated against the given data is ${res.data.result}`)
   }
   return (
-    <div>
-      <input value = {input} placeholder="rule" onChange={e => {setInput(e.target.value)}}></input>
-      <button onClick={ruleSubmitHandler}>Submit</button>
+    <div className="text-center p-5">
+      <p className="text-4xl">Rule AST Evaluator</p>
+      <input className="border-5 border-black p-3 mr-2" value = {input} placeholder="rule" onChange={e => {setInput(e.target.value)}}></input>
+      <button className="bg-blue-400 rounded-md p-3" onClick={ruleSubmitHandler}>Submit</button>
 
       <br/>
-      { ast != {}? <ASTNode node={ast}/> : <></> }
+      <br/>
+      <textarea rows={10} onChange={e => setData(e.target.value)}></textarea>
+      <button className="bg-green-400 rounded-md p-3" onClick={ruleEvaluateHandler}>Evaluate</button>
+
+      <br/>
+      { ast != ""? <ASTNode node={ast}/> : <></> }
 
     </div>
   )
